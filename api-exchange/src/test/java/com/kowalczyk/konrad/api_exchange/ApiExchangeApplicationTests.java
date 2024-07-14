38f0bc5c12a12cbd11f59b6cd261c92c985b3577
Konrad <konrad.kowalczyk.98@gmail.com>
2024-07-14 21:54:14 +0200

    feature/base_config - add api-exchange

---
+++ api-exchange/src/test/java/com/kowalczyk/konrad/api_exchange/ApiExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kowalczyk.konrad.api_exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
