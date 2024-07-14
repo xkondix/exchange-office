f4b518763abd6696713b8de8573c8cdbe50afc52
Konrad <konrad.kowalczyk.98@gmail.com>
2024-07-14 21:55:51 +0200

    feature/base_config - add common

---
+++ common/src/test/java/com/kowalczyk/konrad/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kowalczyk.konrad.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
