2584248464e614d281e070bdedd7107ecbcd55dd
Konrad <konrad.kowalczyk.98@gmail.com>
2024-07-14 14:20:12 +0200

    Init

---
+++ src/main/java/com/kowalczy/konrad/cantor/CantorApplication.java
@@ -0,0 +1,13 @@
+package com.kowalczy.konrad.cantor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CantorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CantorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kowalczy/konrad/cantor/CantorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kowalczy.konrad.cantor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CantorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
