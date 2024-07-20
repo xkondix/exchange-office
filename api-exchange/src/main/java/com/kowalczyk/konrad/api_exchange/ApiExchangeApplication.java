42521e521d6c8612ac73e362745177eacac210c8
Konrad <konrad.kowalczyk.98@gmail.com>
2024-07-20 16:14:30 +0200

    feature/nbp_reactor - adding a reactive call to the nbp for the dollar exchange rate

--- api-exchange/src/main/java/com/kowalczyk/konrad/api_exchange/ApiExchangeApplication.java
+++ api-exchange/src/main/java/com/kowalczyk/konrad/api_exchange/ApiExchangeApplication.java
@@ -3,13 +3,15 @@ package com.kowalczyk.konrad.api_exchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableScheduling
 public class ApiExchangeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiExchangeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiExchangeApplication.class, args);
+    }
 
 }
---
+++ api-exchange/src/main/java/com/kowalczyk/konrad/api_exchange/configuration/WebClientConfig.java
@@ -0,0 +1,18 @@
+package com.kowalczyk.konrad.api_exchange.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${urlExchange}")
+    private String urlExchange;
+
+    @Bean("ExchangeClient")
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.baseUrl(urlExchange).build();
+    }
+}
